c0598bd2c89b5ea5b7d7a49836a19b0aecca05ff
Shlok Vaidya <shlokvaidya.in@gmail.com>
2026-01-01 18:24:30 +0530

    v3.3.2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
       }),
     },
   },
-  plugins: [require("@tailwindcss/typography"), require('@tailwindcss/line-clamp')],
+  plugins: [require("@tailwindcss/typography")],
 };
 
 export default config;
